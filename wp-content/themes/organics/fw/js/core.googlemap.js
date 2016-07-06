725e02dd657ea71d9c0e9535d3b44c577dcc33fe
Gabe L <gabe@fijiwebdesign.com>
2016-07-06 00:48:11 -0500

    Removed debugger

--- wp-content/themes/organics/fw/js/core.googlemap.js
+++ wp-content/themes/organics/fw/js/core.googlemap.js
@@ -36,8 +36,6 @@ function organics_googlemap_init(dom_obj, coords) {
 function organics_googlemap_create(id) {
 	"use strict";
 
-	debugger
-
 	// Create map
 	ORGANICS_GLOBALS['googlemap_init_obj'][id].map = new google.maps.Map(ORGANICS_GLOBALS['googlemap_init_obj'][id].dom, ORGANICS_GLOBALS['googlemap_init_obj'][id].opt);
 
